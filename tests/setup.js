7b34641510b60aec0552ab373f030cbeaf914a71
joel oluwafisayo <122305817+AGBOOLA-JOEL@users.noreply.github.com>
2025-07-18 15:51:52 +0100

    code cleanup

--- tests/setup.js
+++ tests/setup.js
@@ -1,33 +1,3 @@
-// require("dotenv").config(); // Load environment variables from .env at the very top
-// const mongoose = require("mongoose");
-
-// // Set test environment
-// process.env.NODE_ENV = "test";
-// process.env.JWT_SECRET = process.env.JWT_SECRET || "test-secret-key";
-
-// // Connect to test database
-// beforeAll(async () => {
-//   await mongoose.connect(process.env.MONGODB_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-// });
-
-// // Clear database after each test
-// afterEach(async () => {
-//   const collections = mongoose.connection.collections;
-//   for (const key in collections) {
-//     const collection = collections[key];
-//     await collection.deleteMany();
-//   }
-// });
-
-// // Close database connection after all tests
-// afterAll(async () => {
-//   await mongoose.connection.dropDatabase();
-//   await mongoose.connection.close();
-// });
-
 // jest.setTimeout(20000); // 20 seconds
 process.env.JWT_SECRET = process.env.JWT_SECRET || "test-secret-key";
 
